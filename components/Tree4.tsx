1eea46c8f43cd16d50e43fef527862a2ff1c6f52
Linn Strand <linnber@msn.com>
2023-03-27 13:14:31 +0200

    expand on click

--- components/Tree4.tsx
+++ components/Tree4.tsx
@@ -19,11 +19,38 @@ const memberBox = {
 export const Tree4 = () => {
   const [nodes, setNodes] = useState([]);
   const [links, setLinks] = useState([]);
+  const [root, setRoot] = useState(() => {
+    const r = d3.hierarchy(testdata, (person) => person.nodes);
+    r.x0 = dy / 20;
+    r.y0 = 0;
+
+    r.descendants().forEach((node, i) => {
+      node.id = i;
+      node._children = node.children;
+      if (node.depth && node.data.level > 3) {
+        node.children = null;
+      }
+    });
+    return r;
+  });
+
   const [view, setView] = useState(
     `${-margin.left} ${-margin.top} ${width} ${dx}`
   );
 
-  const update = (root) => {
+  const tree = d3
+    .tree()
+    .nodeSize([
+      memberBox.height + memberBox.marginHeight,
+      memberBox.width + memberBox.marginWidth,
+    ])
+    .separation(() => 0.5);
+
+  const update = (source = root) => {
+    tree(root);
+    setNodes(root.descendants().reverse());
+    setLinks(root.links());
+
     let x0 = width;
     let x1 = -width;
     let y0 = startHeight;
@@ -34,14 +61,6 @@ export const Tree4 = () => {
       if (d.y > y1) y1 = d.y;
       if (d.y < y0) y0 = d.y;
     });
-
-    // Stash the old positions for transition.
-    root.eachBefore((d) => {
-      d.x0 = d.x;
-      d.y0 = d.y;
-    });
-    setNodes(root.descendants().reverse());
-    setLinks(root.links());
 
     const height = Math.max(
       x1 - x0 + margin.top + margin.bottom + memberBox.height * 2,
@@ -56,33 +75,19 @@ export const Tree4 = () => {
         x0 - memberBox.height - margin.top
       } ${wwidth} ${height}`
     );
+
+    // Stash the old positions for transition.
+    root.eachBefore((d) => {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
   };
 
   useEffect(() => {
     if (nodes.length > 0) {
       return;
     }
-    const tree = d3
-      .tree()
-      .nodeSize([
-        memberBox.height + memberBox.marginHeight,
-        memberBox.width + memberBox.marginWidth,
-      ])
-      .separation(() => 0.5);
-    const root = d3.hierarchy(testdata, (person) => person.nodes);
-
-    root.x0 = dy / 20;
-    root.y0 = 0;
-
-    root.descendants().forEach((node, i) => {
-      node.id = i;
-      node._children = node.children;
-      if (node.depth && node.data.level > 3) {
-        node.children = null;
-      }
-    });
-    tree(root);
-    update(root);
+    update();
   }, []);
 
   return (
@@ -102,12 +107,12 @@ export const Tree4 = () => {
                     .link<unknown, d3.HierarchyPointNode<any>>(d3.curveStep)
                     .x((d) => d.y)
                     .y((d) => d.x)(n);
-                  console.log(l);
                   return <path key={l} d={l} />;
                 })}
             </g>
             <g className="nodes">
-              {nodes && nodes.map((n) => <Leaf key={n.id} n={n} />)}
+              {nodes &&
+                nodes.map((n) => <Leaf key={n.id} n={n} update={update} />)}
             </g>
           </svg>
         </div>
@@ -116,7 +121,7 @@ export const Tree4 = () => {
   );
 };
 
-const Leaf = ({ n }) => {
+const Leaf = ({ n, update }) => {
   return (
     <g className="node-container" transform={`translate(${n.y}, ${n.x})`}>
       <foreignObject
@@ -130,9 +135,21 @@ const Leaf = ({ n }) => {
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">{n.data.name}</h5>
-            <a onClick={() => update(n)} className="btn btn-primary">
+            <button
+              onClick={() => {
+                if (n.children) {
+                  n._children = [...n.children];
+                  n.children = null;
+                } else if (n._children) {
+                  n.children = [...n._children];
+                  n._children = null;
+                }
+                update(n);
+              }}
+              className="btn btn-primary"
+            >
               expand
-            </a>
+            </button>
           </div>
         </div>
       </foreignObject>
